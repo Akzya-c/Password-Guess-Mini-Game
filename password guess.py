df18b18b72b49bd3d53aa963a8193d7eaa3ec45c
Akshaya <zyaakshu@gmail.com>
2026-02-11 20:57:06 +0530

    Create password guess.py

---
+++ password guess.py
@@ -0,0 +1,32 @@
+import random
+
+def password_puzzle():
+    words = ["code", "data", "loop", "hash", "byte", "node"]
+    password = random.choice(words)
+    attempts = 5
+
+    print("Password Cracking Puzzle (Educational)")
+    print("Hint:", len(password), "-letter lowercase word")
+    print("You have", attempts, "attempts")
+
+    while attempts > 0:
+        guess = input("Enter password guess: ").lower()
+
+        if guess == password:
+            print("Access granted. Password cracked.")
+            return
+        else:
+            correct = 0
+            for i in range(min(len(password), len(guess))):
+                if guess[i] == password[i]:
+                    correct += 1
+
+            print("Incorrect password")
+            print("Correct letters in correct position:", correct)
+
+        attempts -= 1
+        print("Attempts left:", attempts)
+
+    print("Access denied. The password was:", password)
+
+password_puzzle()
